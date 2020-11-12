de91f8e73e60213f39afff97d84fb745be5e2db8
Samantha Silva <samanthaathayde@hotmail.com>
2020-11-12 20:31:51 -0300

    initial commit

---
+++ aula_1/exercicio_2.py
@@ -0,0 +1,9 @@
+"""
+TODO 1: Faça um programa que peça o nome do usuario e o deseje feliz aniversário
+
+Ex.:
+Entrada: Sam
+Saida: Feliz Aniversário, Sam! 
+"""
+
+#TODO 2: Atualize seu programa de forma que, na saída do programa, o nome do usuario sempre tenha a primeira letra maiuscula. 
---
+++ aula_1/exercicio_3.py
@@ -0,0 +1,7 @@
+""" 
+TODO: Peça o nome do usuario e diga a ele quantas letras o nome dele possui.
+
+Ex.: 
+Entrada: Sam
+Saída: "Olá Sam, Seu nome possui 3 letras!"
+"""
